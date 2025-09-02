0dc1b19f1e180c37a2b7ad7a1cc9623ec704a8ae
aaljo222 <125717229+aaljo222@users.noreply.github.com>
2025-09-02 21:21:21 +0900

    Update router.jsx

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,6 +1,7 @@
-// src/routes/Routes.jsx
+// frontend/src/routers/router.jsx
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+
 import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
@@ -8,6 +9,8 @@ import CartPage from "../pages/books/CartPage";
 import CheckoutPage from "../pages/books/CheckoutPage";
 import SingleBook from "../pages/books/SingleBook";
 import PrivateRoute from "./PrivateRoute";
+import OrderPage from "../pages/books/OrderPage";
+
 import AdminRoute from "./AdminRoute";
 import AdminLogin from "../components/AdminLogin";
 import DashboardLayout from "../pages/dashboard/DashboardLayout";
@@ -22,11 +25,36 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      { path: "/", element: <Home /> },
-      { path: "/orders", element: <PrivateRoute><OrderPage /></PrivateRoute> },
-      { path: "/about", element: <div>About</div> },
-      { path: "/login", element: <Login /> },
-      { path: "/register", element: <Register /> },
-      { path: "/cart", element: <CartPage /> },
-      { path: "/checkout", element: <PrivateRoute><CheckoutPage /></PrivateRoute> },
-      { path: "/books/:id", element: <SingleBook /> },
+      { index: true, element: <Home /> },
+      { path: "orders", element: <PrivateRoute><OrderPage /></PrivateRoute> },
+      { path: "about", element: <div>About</div> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "cart", element: <CartPage /> },
+      { path: "checkout", element: <PrivateRoute><CheckoutPage /></PrivateRoute> },
+      { path: "books/:id", element: <SingleBook /> },
+      { path: "user-dashboard", element: <PrivateRoute><UserDashboard /></PrivateRoute> },
+    ],
+  },
+
+  // 관리자 로그인 페이지
+  { path: "/admin", element: <AdminLogin /> },
+
+  // 관리자 영역(부모만 보호)
+  {
+    path: "/dashboard",
+    element: (
+      <AdminRoute>
+        <DashboardLayout />
+      </AdminRoute>
+    ),
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "add-new-book", element: <AddBook /> },
+      { path: "edit-book/:id", element: <UpdateBook /> },
+      { path: "manage-books", element: <ManageBooks /> },
+    ],
+  },
+]);
+
+export default router;
