32293bc760e873dff26f78d1ea8badaa8e0286ef
aaljo222 <125717229+aaljo222@users.noreply.github.com>
2025-09-02 22:04:31 +0900

    Update PrivateRoute.jsx

--- frontend/src/routers/PrivateRoute.jsx
+++ frontend/src/routers/PrivateRoute.jsx
@@ -1,15 +1,10 @@
 // src/routers/PrivateRoute.jsx
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
-const PrivateRoute = ({ children }) => {
-  const { currentUser, loading } = useAuth();
-
+export default function PrivateRoute({ children }) {
+  const { loading, isLoggedIn } = useAuth();
   if (loading) return <div>Loading...</div>;
-  if (!currentUser) return <Navigate to="/login" replace />;
-
+  if (!isLoggedIn) return <Navigate to="/login" replace />;
   return children ? children : <Outlet />;
-};
-
-export default PrivateRoute;
+}
